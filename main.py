3d2bbddc78ad4114bc498865909fab2614e772c1
weichao1221 <wl201808087@icloud.com>
2023-11-04 21:45:34 +0800

    mac

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def get_css():
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app='main:app', host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True)
